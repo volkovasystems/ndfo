456f6cbdb8294063551362a85f8b3ece5b495c60
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-06 11:32:35 +0800

    Improved code.

--- ndfo.js
+++ ndfo.js
@@ -34,6 +34,9 @@
 			"file": "ndfo.js",
 			"module": "ndfo",
 			"author": "Richeve S. Bebedor",
+			"contributors": [
+				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>"
+			],
 			"eMail": "richeve.bebedor@gmail.com",
 			"repository": "https://github.com/volkovasystems/ndfo.git",
 			"test": "ndfo-test.js",
