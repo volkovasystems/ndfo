8184e638837226b48336eb4a3c8b90346f20c86d
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-23 11:05:12 +0800

    Updated client test.

--- ndfo.support.js
+++ ndfo.support.js
@@ -34,10 +34,11 @@
               			"file": "ndfo.js",
               			"module": "ndfo",
               			"author": "Richeve S. Bebedor",
-              			"contributors": [
-              				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>"
-              			],
               			"eMail": "richeve.bebedor@gmail.com",
+              			"contributors": [
+              				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>",
+              				"Vinse Vinalon <vinsevinalon@gmail.com>"
+              			],
               			"repository": "https://github.com/volkovasystems/ndfo.git",
               			"test": "ndfo-test.js",
               			"global": true
